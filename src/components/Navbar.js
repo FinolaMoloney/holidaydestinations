e136936ae8549e4949e2d42b7be90492ce88eda1
Finola Moloney <x22151338@student.ncirl.ie>
2023-04-16 16:08:28 +0100

    update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,23 @@
 function Navbar() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">Holiday Destinations</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-          <div className="dropdown-menu">
-            <a className="dropdown-item" href="/">Home</a>
-            <a className="dropdown-item" href="/hotels">Hotels</a>
-          </div>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">Home</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/hotels">Hotels</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">Holiday Destinations</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+						<a className="nav-link" href="/">Home</a>
+					</li>
+          <li className="nav-item">
+            <a className="nav-link" href="/hotels">Hotels</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
   
   export default Navbar;
   
