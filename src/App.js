0bd3db236944c3cffdb2526d5fa3b0bc60c5e3be
Finola Moloney <x22151338@student.ncirl.ie>
2023-04-22 12:14:11 +0100

    update router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 import Hotels from './views/Hotels';
 import Home from './views/Home';
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        <Switch>
+        
           <Route exact path="/">
             <Home />
           </Route>
@@ -22,7 +22,7 @@ function App() {
           <Route path="/contact">
            <Contact />
           </Route>
-        </Switch>
+        
         <Footer />
       </div>
     </Router>
